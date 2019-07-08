5ee6773b9af0d0f9107533ff779719c61df46b35
armytricks <dhjayalath@gmail.com>
2019-07-08 22:05:46 +1000

    Scoring text and more reliable collision detection

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
+use sdl2::render::TextureQuery;
 use std::time::SystemTime;
 
 const WIDTH: usize = 1280;
@@ -15,10 +16,18 @@ const TITLE: &str = "Outrust";
 const ROWS: u32 = 10;
 const COLS: u32 = 10;
 
+// handle the annoying Rect i32
+macro_rules! rect(
+    ($x:expr, $y:expr, $w:expr, $h:expr) => (
+        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+    )
+);
+
 fn main() {
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let ttf_context = sdl2::ttf::init().unwrap();
  
     let window = video_subsystem.window(TITLE, WIDTH as u32, HEIGHT as u32)
         .position_centered()
@@ -27,6 +36,12 @@ fn main() {
 
     // Build canvas
     let mut canvas = window.into_canvas().build().expect("Problem building canvas from window");
+    let texture_creator = canvas.texture_creator();
+
+    let mut font = ttf_context.load_font("./RobotoMono-Regular.ttf", 128).unwrap();
+    font.set_style(sdl2::ttf::FontStyle::BOLD);
+
+    let mut score: u32 = 0;
 
     sdl_context.mouse().show_cursor(false);
     sdl_context.mouse().capture(true);
@@ -36,7 +51,7 @@ fn main() {
     let mut timer = Timer::new(0, SystemTime::now(), SystemTime::now());
 
     let mut bumper = Bumper::new(ivec2(WIDTH as i32 / 2, HEIGHT as i32 - 10), 0.05, uvec2(100, 10));
-    let mut ball = Ball::new(ivec2(50, 50), dvec2(1.1, 1.1), 10);
+    let mut ball = Ball::new(ivec2(WIDTH as i32 / 2, HEIGHT as i32 - 20), dvec2(0.5, -0.5), 10);
 
     let mut bricks: Vec<Brick> = Vec::with_capacity(100);
 
@@ -68,19 +83,61 @@ fn main() {
         bumper.pos.x = event_pump.mouse_state().x();
 
         // The rest of the game loop goes here...
-        ball.update(timer.frame_time);
+        ball.update(timer.frame_time, &mut score);
         bumper.update(timer.frame_time);
+
         ball_bumper_collision(&bumper, &mut ball);
 
         for j in 0..ROWS {
             for i in 0..COLS {
-                ball_brick_collision(&mut bricks[i as usize + COLS as usize * j as usize], &mut ball);
-                bricks[i as usize + COLS as usize * j as usize].draw(&mut canvas);
+
+                if bricks[i as usize + COLS as usize * j as usize].active {
+                    while bb_collision(&mut bricks[i as usize + COLS as usize * j as usize], &mut ball, &mut score) {
+
+                        ball.pos.x -= bb_dir_x(ball.vel.x) as i32;
+                        if !bb_collision(&mut bricks[i as usize + COLS as usize * j as usize], &mut ball, &mut score) {
+                            if ball.vel.x < 0.0 {
+                                ball.displace(Side::Left);
+                            } else {
+                                ball.displace(Side::Right);
+                            }
+
+                            bricks[i as usize + COLS as usize * j as usize].active = false;
+                            
+                            break
+                        }
+
+                        ball.pos.y -= bb_dir_y(ball.vel.y) as i32;
+                        if !bb_collision(&mut bricks[i as usize + COLS as usize * j as usize], &mut ball, &mut score) {
+                            if ball.vel.y < 0.0 {
+                                ball.displace(Side::Top);
+                            } else {
+                                ball.displace(Side::Bottom);
+                            }
+
+                            bricks[i as usize + COLS as usize * j as usize].active = false;
+
+                            break
+                        }
+
+
+                    }
+                    // ball_brick_collision(&mut bricks[i as usize + COLS as usize * j as usize], &mut ball, &mut score);
+                    bricks[i as usize + COLS as usize * j as usize].draw(&mut canvas);
+                }
             }
         }
 
+        let surface = font.render(&format!("Score: {}", score)[..]).blended(Color::RGB(0xFF, 0xFF, 0xFF)).unwrap();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+        let TextureQuery { width, height, .. } = texture.query();
+        // If the example text is too big for the screen, downscale it (and center irregardless)
+        let padding = 670;
+        let target = get_rect(width, height, WIDTH as u32 - padding, HEIGHT as u32 - padding, 20, 20);
+
         ball.draw(&mut canvas);
         bumper.draw(&mut canvas);
+        canvas.copy(&texture, None, Some(target)).unwrap();
 
         canvas.present();
 
@@ -132,7 +189,7 @@ impl Ball {
             .expect("Problem drawing ball");
     }
 
-    fn update(&mut self, frame_time: u128) {
+    fn update(&mut self, frame_time: u128, score: &mut u32) {
         self.pos.x += (self.vel.x * frame_time as f64) as i32;
         self.pos.y += (self.vel.y * frame_time as f64) as i32;
 
@@ -148,6 +205,7 @@ impl Ball {
             self.displace(Side::Right)
         } else if self.pos.y + self.size as i32 > HEIGHT as i32 - 1 {
             self.pos.y = HEIGHT as i32 - 1 - self.size as i32;
+            *score = 0;
             self.displace(Side::Bottom)
         }
     }
@@ -227,14 +285,56 @@ fn ball_bumper_collision(bumper: &Bumper, ball: &mut Ball) {
     }
 }
 
-fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball) {
+fn bb_collision(brick: &mut Brick, ball: &mut Ball, score: &mut u32) -> bool {
+
+    // Collision x-axis?
+    let collision_x: bool = brick.pos.x + brick.size.x as i32 >= ball.pos.x &&
+        ball.pos.x + ball.size as i32 >= brick.pos.x;
+
+    // Collision y-axis?
+    let collision_y: bool = brick.pos.y + brick.size.y as i32 >= ball.pos.y &&
+        ball.pos.y + ball.size as i32 >= brick.pos.y;
+
+    if collision_x && collision_y {
+        *score += 1;
+        true
+    } else {
+        false
+    }
+}
+
+fn bb_dir_y(vel: f64) -> i32 {
+    if vel > 0.0 {
+        1
+    } else if vel < 0.0 {
+        -1
+    } else {
+        0
+    }
+}
+
+fn bb_dir_x(vel: f64) -> i32 {
+    if vel > 0.0 {
+        1
+    } else if vel < 0.0 {
+        -1
+    } else {
+        0
+    }
+}
+
+fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball, score: &mut u32) {
 
     if brick.active {
+
+        let norm = normalize(ball.vel);
 
         if ball.pos.y >= brick.pos.y && ball.pos.y <= brick.pos.y + brick.size.y as i32 {
 
             if ball.pos.x >= brick.pos.x && ball.pos.x <= brick.pos.x + brick.size.x as i32 {
                 brick.active = false;
+                *score += 1;
+                ball.vel = norm * (1.0 + *score as f64 / 100.0);
                 if ball.vel.y < 0.0 {
                     ball.displace(Side::Top)
                 } else {
@@ -242,6 +342,8 @@ fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball) {
                 }
             } else if ball.pos.x + ball.size as i32 >= brick.pos.x && ball.pos.x + ball.size as i32 <= brick.pos.x + brick.size.x as i32 {
                 brick.active = false;
+                *score += 1;
+                ball.vel = norm * (1.0 + *score as f64 / 100.0);
                 if ball.vel.y < 0.0 {
                     ball.displace(Side::Top)
                 } else {
@@ -253,6 +355,8 @@ fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball) {
 
             if ball.pos.x >= brick.pos.x && ball.pos.x <= brick.pos.x + brick.size.x as i32 {
                 brick.active = false;
+                *score += 1;
+                ball.vel = norm * (1.0 + *score as f64 / 100.0);
                 if ball.vel.y < 0.0 {
                     ball.displace(Side::Top)
                 } else {
@@ -260,6 +364,8 @@ fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball) {
                 }
             } else if ball.pos.x + ball.size as i32 >= brick.pos.x && ball.pos.x + ball.size as i32 <= brick.pos.x + brick.size.x as i32 {
                 brick.active = false;
+                *score += 1;
+                ball.vel = norm * (1.0 + *score as f64 / 100.0);
                 if ball.vel.y < 0.0 {
                     ball.displace(Side::Top)
                 } else {
@@ -277,4 +383,26 @@ fn ball_brick_collision(brick: &mut Brick, ball: &mut Ball) {
 
     // Right side
 
+}
+
+// Scale fonts to a reasonable size when they're too big (though they might look less smooth)
+fn get_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_height: u32, x: u32, y: u32) -> Rect {
+    let wr = rect_width as f32 / cons_width as f32;
+    let hr = rect_height as f32 / cons_height as f32;
+
+    let (w, h) = if wr > 1f32 || hr > 1f32 {
+        if wr > hr {
+            println!("Scaling down! The text will look worse!");
+            let h = (rect_height as f32 / wr) as i32;
+            (cons_width as i32, h)
+        } else {
+            println!("Scaling down! The text will look worse!");
+            let w = (rect_width as f32 / hr) as i32;
+            (w, cons_height as i32)
+        }
+    } else {
+        (rect_width as i32, rect_height as i32)
+    };
+
+    rect!(x, y, w, h)
 }
